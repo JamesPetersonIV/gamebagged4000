b1697d712eb4e3fb91d26ef5085e9c7551f03c04
James Peterson IV <61022550+JamesPetersonIV@users.noreply.github.com>
2022-12-03 18:49:45 -0500

    12/3/2022 6:49PM Errors caught by Kamari Young

--- gamebagged/accounts/urls.py
+++ gamebagged/accounts/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('products/', views.products, name='products'),
+    #path('products/', views.products, name='products'),
     path('customer/<int:pk>/', views.customer, name='customer'),
     path('order/', PlaceOrder.as_view(), name='order'),
     path('update_order/<int:pk>/', views.cusupdateOrder, name='update_order'),
